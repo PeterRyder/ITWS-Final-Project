1673801d8672c5f46f86a2753c2e95d64f1caf7c
PeterRyder1 <peter.w.ryder@gmail.com>
2014-11-17 16:55:55 -0500

    removed yelp get data js file - migrated to get_location

--- get_location.js
+++ get_location.js
@@ -65,13 +65,14 @@ function currentPosition(position) {
     check_initial = true;
     getmap();
     getInsta();
+    getYelp();
   }
   if (diff > update_delay) {
     codeLatLng();
     check_time = now;
     getmap();
-
     getInsta();
+    getYelp();
   }
   amount_checks += 1;
   $('#checks').html('Checks: ' + amount_checks);
@@ -90,7 +91,7 @@ function initialize() {
 function codeLatLng() {
   var lat = parseFloat(current_lat);
   var lng = parseFloat(current_long);
-
+  
   latlng = new google.maps.LatLng(lat, lng);
   geocoder.geocode({
     'latLng': latlng
@@ -146,5 +147,70 @@ function getInsta() {
       $("#instagram ul").html(msg);
     }
   });
+}
+
+function getYelp() {
+  var auth = {
+    //
+    // Update with your auth tokens.
+    //
+    consumerKey: "6bYY1U8dhgFt17noBW5NXQ",
+    consumerSecret: "LXEV5iVj-Azb12ub7G8kaFWpK5Q",
+    accessToken: "tX6UzxaDaewFyriSB902mFFs4EDUF6Lx",
+    // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+    // You wouldn't actually want to expose your access token secret like this in a real application.
+    accessTokenSecret: "YpjvnyGiU5JRb6RaPrl0LmmVTjg",
+    serviceProvider: {
+      signatureMethod: "HMAC-SHA1"
+    }
+  };
+
+  var accessor = {
+    consumerSecret: auth.consumerSecret,
+    tokenSecret: auth.accessTokenSecret
+  };
+
+  parameters = [];
+  parameters.push(['callback', 'cb']);
+  parameters.push(['oauth_consumer_key', auth.consumerKey]);
+  parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+  parameters.push(['oauth_token', auth.accessToken]);
+  parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+  var message = {
+    'action': 'http://api.yelp.com/v2/search?term=restaurants&location=' + city + '+' + state + '&limit=10',
+    'method': 'GET',
+    'parameters': parameters
+  };
+
+  OAuth.setTimestampAndNonce(message);
+  OAuth.SignatureMethod.sign(message, accessor);
+  var parameterMap = OAuth.getParameterMap(message.parameters);
+  parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
+
+  $.ajax({
+    'url': message.action,
+    'data': parameterMap,
+    'cache': true,
+    'dataType': 'jsonp',
+    'jsonpCallback': 'cb',
+    'success': function (data, textStats, XMLHttpRequest) {
+
+      var return_data = "<ul>";
+
+      var businesses = data["businesses"];
+
+      for (var i in businesses) {
+        var business = businesses[i];
+
+        return_data += "<li>" + "<div class='listings'>" + "<span class='text'>" + '<a target="_blank" href="' + business["url"] + '">' + business["name"] + '</a>' + "</span>" + "<span class='rating'>" + " Rating: " + '<img src="' + business["rating_img_url"] + '"' + "</span>" + "</div>" + "</li>";
+      }
+
+      return_data += "</ul>"
+
+      $("#restaurants").html(return_data);
+
+    }
+  }); 
 }
 
