af26c560beb3addcffaa8a789a4f518d8316cb2a
Devin Nguyen <nguyed5@rpi.edu>
2014-11-20 02:07:27 -0500

    Setting up file for pulling session data into JS

---
+++ fetch_session.php
@@ -0,0 +1,13 @@
+<?php
+
+// Used to get the session variable data and send 
+// back to JQuery via AJAX
+
+// Three variables: Latitude, Longitude, Custom Location Enabled/Disabled
+
+// Check if session variables exist first, pull the data,
+// send back. 
+
+
+
+?>
